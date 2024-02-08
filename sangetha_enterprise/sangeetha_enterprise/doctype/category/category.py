faceadbd78ec3141f9e8c2ea07dfea78234f88a1
sibikumarkuppusamy <sibi.kumar@thrivusoft.in>
2024-02-08 11:18:08 +0530

    Category doctype

---
+++ sangetha_enterprise/sangeetha_enterprise/doctype/category/category.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Thirvusoft Private Limited and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class Category(NestedSet):
+	pass
